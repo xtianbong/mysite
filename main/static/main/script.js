2db4978376b7345c887e329e8aca5323b5f1022a
xtianbong <christianmbong45@gmail.com>
2022-12-03 21:52:24 -0500

    Home section pretty much done. Added js for active nav-item. Portfolio section modified a bit, needs some more work.

--- main/static/main/script.js
+++ main/static/main/script.js
@@ -1,4 +1,4 @@
-window.onscroll=()=>{
+/*window.onscroll=()=>{
     var current=""
     const sections = document.querySelectorAll('.testimonial');
     s_array=>Array.from(sections).forEach((section,index)=>{
@@ -14,4 +14,26 @@ window.onscroll=()=>{
             li.classList.add("active");
         }
     });
-};
+};*/
+
+const sections = document.querySelectorAll(".section");
+console.log(sections)
+const navLi = document.querySelectorAll('.nav-item');
+console.log(navLi)
+window.addEventListener('scroll', ()=>{
+    let current='';
+    sections.forEach( section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight=section.clientHeight;
+        if (pageYOffset>=sectionTop){
+            current=section.getAttribute('id')
+        }
+    })
+    console.log(current)
+    navLi.forEach(li=>{
+        li.classList.remove('active');
+        if(li.classList.contains(current)){
+            li.classList.add('active')
+        }
+    })
+})
